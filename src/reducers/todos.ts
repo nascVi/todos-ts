203fec3021dd8ed4d55e3ad5fdcdef768948c864
nascvi <vaiiho.nascimbene@gmail.com>
2020-12-11 08:50:11 +0100

    initial todos reducers config

---
+++ src/reducers/todos.ts
@@ -0,0 +1,10 @@
+import { Todo, Action, ActionTypes } from '../actions';
+
+export const todosReducer = (state: Todo[] = [], action: Action) => {
+  switch (action.type) {
+    case ActionTypes.fetchTodos:
+      return action.payload
+    default:
+      return state;
+  }
+}
