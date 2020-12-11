8df0cf9193ee11414b5857a97d9292b01a53aafe
nascvi <vaiiho.nascimbene@gmail.com>
2020-12-11 11:33:02 +0100

    🧪 TEST:

--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -3,7 +3,9 @@ import { Todo, Action, ActionTypes } from '../actions';
 export const todosReducer = (state: Todo[] = [], action: Action) => {
   switch (action.type) {
     case ActionTypes.fetchTodos:
-      return action.payload
+      return action.payload;
+    case ActionTypes.deleteTodo:
+      return state.filter((todo: Todo) => todo.id !== action.payload);
     default:
       return state;
   }
